f32affceac269926cb1593cb6b0707a2914fee15
Drake <Drakemcgillivray@cmail.carleton.ca>
2024-05-09 14:15:49 -0400

    https://github.com/Drake-mcg21/NewHMIProject.git

---
+++ ProjectFiles/NetSolution/First_run_time.cs
@@ -0,0 +1,31 @@
+#region Using directives
+using System;
+using UAManagedCore;
+using OpcUa = UAManagedCore.OpcUa;
+using FTOptix.HMIProject;
+using FTOptix.Retentivity;
+using FTOptix.UI;
+using FTOptix.NativeUI;
+using FTOptix.CoreBase;
+using FTOptix.Core;
+using FTOptix.NetLogic;
+using FTOptix.Alarm;
+using FTOptix.SQLiteStore;
+using FTOptix.Store;
+using FTOptix.EventLogger;
+using FTOptix.WebUI;
+using FTOptix.Recipe;
+#endregion
+
+public class First_run_time : BaseNetLogic
+{
+    public override void Start()
+    {
+        // Insert code to be executed when the user-defined logic is started
+    }
+
+    public override void Stop()
+    {
+        // Insert code to be executed when the user-defined logic is stopped
+    }
+}
